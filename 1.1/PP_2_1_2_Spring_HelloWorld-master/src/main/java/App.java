cd5b4265f3095ad670c192f5b87b63fe0886c50f
RyanEnd19 <olisov_vbk@bk.ru>
2023-05-02 19:18:52 +0300

    Initial commit

---
+++ 1.1/PP_2_1_2_Spring_HelloWorld-master/src/main/java/App.java
@@ -0,0 +1,17 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld beanHelloWorld =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHelloWorld.getMessage());
+
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1 == beanCat2);
+    }
+}
